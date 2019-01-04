85a39713da9467729d1bf376b1e93567ad2a4316
Trung Nguyen <ngthanhtrung23@gmail.com>
2019-01-04 16:30:55 +0800

    Improve error messages and reorganize some code.

--- verify.py
+++ verify.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 import re
 import subprocess
 import sys
@@ -5,6 +7,7 @@ import yaml
 
 from argparse import ArgumentParser
 from pathlib import Path
+from termcolor import colored
 
 COMPILE_COMMAND = "g++-8 %(code_path)s " \
                   "--std=c++14 -O2 " \
@@ -16,6 +19,28 @@ EPS = 10 ** -6
 # Clear last line in terminal
 CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
+
+# For printing verification status
+TICK = '✔'
+CROSS = '✘'
+
+# Input and output file format
+INPUT_SUFFIX = ".inp"
+OUTPUT_SUFFIX = ".out"
+
+
+def verification_status(message: str, success: bool):
+    sign = TICK if success else CROSS
+    color = 'green' if success else 'red'
+    print("[" + colored(sign, color) + "] " + message)
+
+
+def verification_success(message: str):
+    verification_status(message, True)
+
+
+def verification_failed(message: str):
+    verification_status(message, False)
 
 
 class Test:
@@ -34,9 +59,12 @@ class Subtask:
         self.tests = []
         compiled_regex = re.compile(regex)
         for filename in tests_path.iterdir():
-            if compiled_regex.match(filename.name) and filename.suffix == ".inp":
+            if compiled_regex.match(filename.name) and filename.suffix == INPUT_SUFFIX:
                 test_name = filename.name[:filename.name.find('.')]
-                test = Test(tests_path, input=test_name + ".inp", output=test_name + ".out", subtask_id=subtask_id)
+                test = Test(tests_path,
+                            input=test_name + INPUT_SUFFIX,
+                            output=test_name + OUTPUT_SUFFIX,
+                            subtask_id=subtask_id)
                 self.tests.append(test)
 
     def __str__(self):
@@ -49,13 +77,8 @@ class Problem:
         try:
             self.path = Path(relative_path)
         except FileNotFoundError:
-            raise ValueError("Problem path does not exist: '%s'", relative_path)
-        print("Verify problem at '%s'" % self.path.resolve())
-
-        # self.tests_path = Path to test directory.
-        self.tests_path = self.path / "tests"
-        if not self.tests_path.is_dir():
-            raise ValueError("Test directory not found. Please rename test dir to 'tests'")
+            raise ValueError("Problem dir does not exist: '%s'", relative_path)
+        verification_success("Problem dir found at %s" % self.path.resolve())
 
         # self.config_path = Path to config file.
         self.config_path = self.path / "config.yaml"
@@ -65,19 +88,24 @@ class Problem:
             except yaml.YAMLError as err:
                 raise ValueError("Could not load config file %s", str(err))
 
-        # self.submission_path = Path to submission directory.
-        self.submission_path = self.path / "submissions"
-        if not self.submission_path.is_dir():
-            raise ValueError("Submission directory not found. Please name it 'submissions'")
+        # self.tests_path = Path to test directory.
+        self.tests_path = self.path / "tests"
+        if not self.tests_path.is_dir():
+            raise ValueError("Test directory not found. Please rename test dir to 'tests'")
 
         # self.subtasks
-        self.verify_tests()
-        print('Subtasks:')
         self.subtasks = []
         for subtask in self.config['subtasks']:
             sub = Subtask(self.tests_path, subtask['regex'], subtask['score'], int(subtask['id']))
-            print("- ", sub)
             self.subtasks.append(sub)
+        verification_success(
+            "%d subtasks, scores = %s" % (len(self.subtasks), [subtask.score for subtask in self.subtasks]))
+
+        # self.submission_path = Path to submission directory.
+        self.submission_path = self.path / "submissions"
+        if not self.submission_path.is_dir():
+            raise ValueError("Submission dir not found. Please name it 'submissions'")
+        verification_success("Submission dir found.")
 
         # self.verifier
         if 'checker' in self.config['problem']:
@@ -85,32 +113,52 @@ class Problem:
             if not self.verifier_path.exists():
                 raise ValueError("Output checker not found: %s" % self.config['problem']['checker'])
 
-            print("Compiling checker..")
             self.verifier_exec_path = Path("./tmp") / "checker"
             compile_cpp(self.verifier_path, self.verifier_exec_path)
+            verification_success("Found and compiled checker %s" % self.config['problem']['checker'])
         else:
             self.verifier_path = None
             self.verifier_exec_path = None
+            verification_success("No checker required. Using default diff -w")
 
     def verify_tests(self):
         """
         Make sure all tests have input + output.
         """
 
+        # Verify that number of input file == number of output file.
         cnt_input = count_file_with_extension(self.tests_path, "inp")
         cnt_output = count_file_with_extension(self.tests_path, "out")
         if cnt_input != cnt_output:
-            print("ERROR: Number of input and output files NOT match: Found %s input and %s output", cnt_input,
-                  cnt_output)
+            verification_failed("ERROR: Number of input and output files NOT match: Found %s input and %s output" % (
+                cnt_input, cnt_output))
 
-        print("Found %s tests" % cnt_input)
-        # TODO: check for each input, output exists.
+        verification_success("Found %s tests" % cnt_input)
 
-    def verify_subtask_scores(self):
+        # Verify that the set of file names of all inputs matches the set of file names of all outputs.
+        input_file_names = set(
+            [filename.with_suffix('').name for filename in self.tests_path.iterdir() if
+             filename.suffix == INPUT_SUFFIX])
+        output_file_names = set(
+            [filename.with_suffix('').name for filename in self.tests_path.iterdir() if
+             filename.suffix == OUTPUT_SUFFIX])
+        if input_file_names == output_file_names:
+            verification_success("Input and output file names match")
+        else:
+            verification_failed("Input and output file names not match:\nIn - Out = %s\nOut - In = %s" % (
+                input_file_names.difference(output_file_names), output_file_names.difference(input_file_names)))
+
+    def verify_subtasks(self):
+        # Verify total score of all subtask == problem score.
         total_score = sum(subtask.score for subtask in self.subtasks)
         if total_score != int(self.config['problem']['score']):
-            print("ERROR: Total score of all subtask = %d, does not match problem config's total score = %d"
-                  % (total_score, self.config['problem']['score']))
+            verification_failed("Total score of all subtask = %d, NOT matching problem config's total score = %d"
+                                % (total_score, self.config['problem']['score']))
+
+        # Verify each subtask has at least 1 test.
+        for subtask in self.subtasks:
+            if len(subtask.tests) == 0:
+                verification_failed("Subtask %d has 0 tests" % subtask.subtask_id)
 
     def verify_submissions(self):
         """
@@ -118,12 +166,12 @@ class Problem:
         """
 
         if 'solutions' not in self.config:
-            print("ERROR: No solutions found")
+            verification_failed("No solutions found")
             return
 
         for submission in self.config['solutions']:
             filename = str(submission['name'])
-            print("Judging %s" % filename)
+            print("Running %s" % filename)
             code_path = self.submission_path / filename
             exec_path = Path("./tmp") / filename[:filename.find('.')]
 
@@ -134,10 +182,11 @@ class Problem:
             max_score = submission['max_score']
 
             if score < min_score - EPS:
-                print("ERROR: %s received %f, min_score = %f" % (filename, score, min_score))
-
-            if score > max_score + EPS:
-                print("ERROR: %s received %f, max_score = %f" % (filename, score, max_score))
+                verification_failed("%s received %f, min_score = %f" % (filename, score, min_score))
+            elif score > max_score + EPS:
+                verification_failed("%s received %f, max_score = %f" % (filename, score, max_score))
+            else:
+                verification_success("%s received %f, in range [%f, %f]" % (filename, score, min_score, max_score))
 
     def judge_exec(self, exec_path: Path) -> float:
         """
@@ -150,7 +199,7 @@ class Problem:
             print("- Subtask: ", subtask.regex)
 
             if len(subtask.tests) == 0:
-                print("WARNING: Subtask has 0 tests")
+                # Note that this is already checked in verify_subtasks, so we just skip and do not print anything here.
                 continue
 
             print("  Running on tests..")
@@ -172,7 +221,6 @@ class Problem:
             print("  Subtask score = %f" % subtask_score)
             score += subtask_score
 
-        print("%s has total score = %f" % (exec_path.name, score))
         return score
 
     def verify_output(self, test: Test, output_path: Path) -> bool:
@@ -215,7 +263,7 @@ def compile_cpp(code_path: Path, exec_path: Path):
     try:
         output = subprocess.run(command, stderr=subprocess.STDOUT, shell=True)
     except subprocess.CalledProcessError as e:
-        print("ERROR: Compile error for %s" % code_path.resolve())
+        verification_failed("ERROR: Compile error for %s" % code_path.resolve())
         print(e)
 
         if output is not None:
@@ -244,7 +292,7 @@ def run_code(exec_path: Path, input_path: Path, output_path: Path, time_limit_se
             stream.write(output.stdout)
         return True
     except subprocess.CalledProcessError as e:
-        print("ERROR: Compile error for %s" % exec_path.resolve())
+        verification_failed("ERROR: Execution error for %s" % exec_path.resolve())
         print(e)
         if output is not None:
             print("------")
@@ -269,10 +317,11 @@ def main():
     try:
         problem = Problem(args.dir)
     except ValueError as e:
-        print("ERROR: %s" % str(e))
+        verification_failed("ERROR: %s" % str(e))
         return
 
-    problem.verify_subtask_scores()
+    problem.verify_tests()
+    problem.verify_subtasks()
     problem.verify_submissions()
 
 
